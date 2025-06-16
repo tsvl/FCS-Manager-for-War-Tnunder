5d1d862b4c1fa4ca403d727b32d3632ce541e111
tsvl <67887268+tsvl@users.noreply.github.com>
2025-06-16 16:46:10 -0700

    fix localisation/datamine path oops

--- Source/Form1.Designer.cs
+++ Source/Form1.Designer.cs
@@ -106,7 +106,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(241, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Localisation/Datamine";
+            this.textBox1.Text = "Localisation";
             this.textBox1.Click += new System.EventHandler(this.TextBox1_Click);
             // 
             // button1
